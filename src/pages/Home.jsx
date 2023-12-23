e29e9fb1c8780f393b1c389c097650b659ffa59a
Mapur2 <rupammodak1@gmail.com>
2023-12-23 17:44:41 +0530

    Initial Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Intro from '../components/Intro'
+
+function Home() {
+  return (
+    <div>
+      <Hero/>
+      <Intro/>
+    </div>
+  )
+}
+
+export default Home
